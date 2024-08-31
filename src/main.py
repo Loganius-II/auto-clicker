7e1d6b1f5bd9b1c85eebca993b82a321b3710727
Loganius-II <132830555+Loganius-II@users.noreply.github.com>
2024-08-31 17:17:26 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,9 +1,7 @@
-import pyautogui as pag
 from time import sleep
 import win32con, win32api
-import subprocess
-import os
 import keyboard
+import pygame
 
 def click(x: str | float, y: str | float) -> None:
     win32api.SetCursorPos((x,y))
@@ -18,18 +16,8 @@ def tipe(message: str, speed: float = 0.03):
     #start new line
     print()
 
-def main() -> None:
-    tipe("AUTO-GAME-CLICKER")
-    tipe("author: Logan McDermott")
-    tipe("Popular uses:\n1. To keep mouse active during work\n2. Banana steam game")
-    tipe("3. Other clicker games")
-    tipe("Enter to continue...")
-    input()
-    os.system('cls') if os.name == 'nt' else os.system('clear')
-    tipe("Put your cursor where you want to rapid click\nPress Enter when you are ready...")
-    input()
+def clicker() -> None:
     mouse_x, mouse_y = win32api.GetCursorPos()
-    tipe("Clicking...\nPress E to exit")
     while True:
         if keyboard.is_pressed('e'):
             break
@@ -41,6 +29,15 @@ def main() -> None:
             break
         except Exception as e:
             print(e)
+            break
+
+def main() -> None:
+    print("Press Q to start, E to stop, and ` to exit code")
+    while True:
+        if keyboard.is_pressed('q'):
+            clicker()
+
+        if keyboard.is_pressed("`"):
             break
 if __name__ == main():
     main()
